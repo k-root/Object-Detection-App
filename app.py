6581d70fb63817cbf0e8e4cd7eb963c546da80d0
Kaushik Koilada <kkoilada@digitalriver.com>
2020-02-18 19:48:55 +0530

    flask app object detection

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask
+from flask import render_template, Blueprint, send_from_directory, request
+from flask_cors import CORS
+import requests
+import re
+import json
+import train
+
+import os
+app = Flask(__name__)
+app.config["DEBUG"]=True
+import tensorflow as tf
+import pathlib
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+def load_model(model_name):
+  base_url = 'http://download.tensorflow.org/models/object_detection/'
+  model_file = model_name + '.tar.gz'
+  model_dir = tf.keras.utils.get_file(
+    fname=model_name, 
+    origin=base_url + model_file,
+    untar=True)
+
+  model_dir = pathlib.Path(model_dir)/"saved_model"
+
+  print(model_dir)
+  model = model = tf.compat.v2.saved_model.load(str(model_dir))
+  model = model.signatures['serving_default']
+
+  return model
+ 
+
+@app.route("/train",methods=['GET', 'POST'])
+def runTrain():
+    modelName = request.args.get("modelName")
+    dirList = os.listdir("./")
+    # if modelName in dirList:
+    #     #load model
+    #     pass
+    # else:
+    #     #download from zoo
+    #     pass
+    model = load_model(modelName)
+    print(model)
+
+    print(dirList)
+    model_dir = "training"
+    pipeline_config_path = "training/ssd_mobilenet_v2_coco.config"
+    num_train_steps = None
+    eval_training_data=False
+    if not eval_training_data:
+        checkpoint_dir=None
+    else:
+        os.mkdir("checkpoint/"+modelName)
+    if model_dir not in dirList or not os.path.isdir(model_dir):
+        os.mkdir("training")
+    
+    train.training(model_dir,pipeline_config_path,num_train_steps, eval_training_data, checkpoint_dir)
+    
+    return "train"+modelName
+
+
+@app.route("/test",methods=['GET', 'POST'])
+def testModel():
+    modelName = request.args.get("modelName")
+    return "test"+modelName
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ train.py
@@ -0,0 +1,83 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Binary to run train and evaluation on object detection model."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl import flags
+
+import tensorflow as tf
+
+import model_hparams
+import model_lib
+
+def training(model_dir, pipeline_config_path, num_train_steps, eval_training_data, checkpoint_dir, sample_1_of_n_eval_examples=1, sample_1_of_n_eval_on_train_examples=5, hparams_overrides=None, run_once=False):
+#   model_dir
+#   pipeline_config_path
+#   num_train_steps
+#   eval_training_data
+#   ######'''should be optional'''######
+#   sample_1_of_n_eval_examples  
+#   sample_1_of_n_eval_on_train_examples
+#   hparams_overrides
+#   checkpoint_dir
+#   run_once
+  config = tf.estimator.RunConfig(model_dir=model_dir)
+
+  train_and_eval_dict = model_lib.create_estimator_and_inputs(
+      run_config=config,
+      hparams=model_hparams.create_hparams(hparams_overrides),
+      pipeline_config_path=pipeline_config_path,
+      train_steps=num_train_steps,
+      sample_1_of_n_eval_examples=sample_1_of_n_eval_examples,
+      sample_1_of_n_eval_on_train_examples=(
+          sample_1_of_n_eval_on_train_examples))
+  estimator = train_and_eval_dict['estimator']
+  train_input_fn = train_and_eval_dict['train_input_fn']
+  eval_input_fns = train_and_eval_dict['eval_input_fns']
+  eval_on_train_input_fn = train_and_eval_dict['eval_on_train_input_fn']
+  predict_input_fn = train_and_eval_dict['predict_input_fn']
+  train_steps = train_and_eval_dict['train_steps']
+
+  if checkpoint_dir:
+    if eval_training_data:
+      name = 'training_data'
+      input_fn = eval_on_train_input_fn
+    else:
+      name = 'validation_data'
+      # The first eval input will be evaluated.
+      input_fn = eval_input_fns[0]
+    if run_once:
+      estimator.evaluate(input_fn,
+                         steps=None,
+                         checkpoint_path=tf.train.latest_checkpoint(
+                             checkpoint_dir))
+    else:
+      model_lib.continuous_eval(estimator, checkpoint_dir, input_fn,
+                                train_steps, name)
+  else:
+    train_spec, eval_specs = model_lib.create_train_and_eval_specs(
+        train_input_fn,
+        eval_input_fns,
+        eval_on_train_input_fn,
+        predict_input_fn,
+        train_steps,
+        eval_on_train_data=False)
+
+    # Currently only a single Eval Spec is allowed.
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_specs[0])
+    
