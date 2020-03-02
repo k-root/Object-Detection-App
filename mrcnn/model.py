a59b9d9deebf0957cdb4343fb16c26537f7c5a1c
Kaushik Koilada <kkoilada@digitalriver.com>
2020-03-02 11:12:19 +0530

    checkpoints overwrite with most recent checkpoint

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2268,10 +2268,13 @@ class MaskRCNN():
             self.config.NAME.lower(), now))
 
         # Path to save after each epoch. Include placeholders that get filled by Keras.
-        self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
-            self.config.NAME.lower()))
-        self.checkpoint_path = self.checkpoint_path.replace(
-            "*epoch*", "{epoch:04d}")
+        # self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
+        #     self.config.NAME.lower()))
+        # self.checkpoint_path = self.checkpoint_path.replace(
+        #     "*epoch*", "{epoch:04d}")
+
+        self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}.h5".format(
+        self.config.NAME.lower()))
 
     def train(self, train_dataset, val_dataset, learning_rate, epochs, layers,
               augmentation=None, custom_callbacks=None, no_augmentation_sources=None):
